6a6ffc9a9dc2133d9503dfec5ff24e2547c05bf1
Ruslan Salikhov <ruslan.salihov@gmail.com>
2018-04-02 14:36:08 +0500

    Added docker compose

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker-compose stop
 docker rmi karma-faucet
 docker build -t karma-faucet .
-docker run -p 9090:9090/tcp -it --rm --name karma-faucet karma-faucet
+#docker run -p 9090:9090/tcp -it --rm --name karma-faucet karma-faucet
+docker-compose up -d
+
